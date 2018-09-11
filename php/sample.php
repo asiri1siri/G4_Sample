d6d48cb47ede901e7044611dd850650f9670b443
asiri1siri <asiri1siri@gmail.com>
2018-09-11 15:52:31 -0700

    base php connection files

---
+++ php/sample.php
@@ -0,0 +1,24 @@
+<?php
+$servername = "localhost";
+$username = "username";
+$password = "";
+$dbname = "test_base";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "INSERT INTO users (ID,FirstName, LastName, Email)
+VALUES ('45128', 'John', 'Doe', 'john@example.com')";
+
+if (mysqli_query($conn, $sql)) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+?>
