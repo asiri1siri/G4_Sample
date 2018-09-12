23477e84c55eacd38e18b43482b9d987d0a785f9
asiri1siri <asiri1siri@gmail.com>
2018-09-11 17:28:30 -0700

    fully functioning CRUD with CSS

---
+++ php/working/connect-db.php
@@ -0,0 +1,12 @@
+<?php
+
+// server info
+$server = 'localhost';
+$user = 'root';
+$pass = '';
+$db = 'test_base';
+
+// connect to the database
+$mysqli = new mysqli($server, $user, $pass, $db);
+
+?>
