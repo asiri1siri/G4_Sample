4ff0a6e17feaefeac990fb670f4d70be8daf9f3c
georgechan74 <georgechan74@gmail.com>
2018-09-13 19:57:31 -0700

    updated enable/disable users

--- php_files/DisableUser.php
+++ php_files/DisableUser.php
@@ -1,17 +1,20 @@
 <?php
 
+    include("connectToDB.php");
+
     //if id is not null
-    if (is_null($_POST['id']))
+    if (is_null($_GET['id']))
     {
         header("Location: Display.php");
     }
     else
     {
         //1. get id to disable
-        $idToDisable = $_POST['id'];
+        $idToDisable = $_GET['id'];
 
         //2. disable user
-        mysql_query("update 'table-name' set '' = '' where id = $idToDisable");
+        $sql = "update users set enabled = 0 where id = " . $idToDisable;
+        mysql_query($sql);
 
         //3. redirect back to display
         header("Location: Display.php");
--- php_files/EnableUser.php
+++ php_files/EnableUser.php
@@ -1,17 +1,20 @@
 <?php
 
+    include("connectToDB.php");
+
     //if id is not null
-    if (is_null($_POST['id']))
+    if (is_null($_GET['id']))
     {
         header("Location: Display.php");
     }
     else
     {
         //1. get id to enable
-        $idToEnable = $_POST['id'];
+        $idToEnable = $_GET['id'];
 
         //2. enable user
-        mysql_query("update 'table-name' set '' = '' where id = $idToEnable");
+        $sql = "update users set enabled = 1 where id = " . $idToEnable;
+        mysql_query($sql);
 
         //3. redirect back to display
         header("Location: Display.php");
