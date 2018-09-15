66e3f27bdad76a0aaa76f901d90e4c0d91d3dd0f
georgechan74 <georgechan74@gmail.com>
2018-09-14 17:34:28 -0700

    updated remove/unremove items

--- php_files/RemoveItem.php
+++ php_files/RemoveItem.php
@@ -1,13 +1,17 @@
 <?php
 
+    include("connectToDB.php");
+
     //if id is not null and is a number
-    if (is_numeric($_POST['id']) && !is_null($_POST['id']))
+    if (is_numeric($_GET['id']) && !is_null($_GET['id']))
     {
         //1. get id to 'remove'
-        $idToRemove = $_POST['id'];
+        $idToRemove = $_GET['id'];
 
         //2. command to change to hidden
-        mysql_query("update 'table-name' set '' = '' where id = $idToRemove");
+        //mysql_query("update 'table-name' set '' = '' where id = $idToRemove");
+        $sql = "update items set deleted=1 where id = " . $idToRemove;
+        $conn->query($sql);
 
         //3. redirect back to display (Display.php is a placeholder)
         header("Location: Display.php");
