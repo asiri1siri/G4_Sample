817513260c43f0553b4443ae05a1f41847a98238
bdebilzan <bdebilzan@mac.com>
2018-09-27 15:09:36 -0700

    updated w/ login and logout

---
+++ practice/logout.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Index</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+</head>
+<body>
+
+<h1>You have successfully logged out.</h1>
+
+<p><a href = "login.php"> Log back in</a></p>
+
+</body>
+</html>
